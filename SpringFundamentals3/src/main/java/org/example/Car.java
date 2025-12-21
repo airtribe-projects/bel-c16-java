8499cc9d9964f15d313c892f9a5b122812f611b3
piyushpawan0000 <34138285+piyushpawan0000@users.noreply.github.com>
2025-12-21 14:40:34 +0530

    Add files via upload

---
+++ SpringFundamentals3/src/main/java/org/example/Car.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Car {
+  private String color;
+
+  private String model;
+
+  private Engine engine;
+
+  public Car(@Value("${car.color}") String color, @Value("${car.model}") String model, Engine engine) {
+    this.color = color;
+    this.model = model;
+    this.engine = engine;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public Engine getEngine() {
+    return engine;
+  }
+
+  public void setEngine(Engine engine) {
+    this.engine = engine;
+  }
+
+  public void printCar() {
+    System.out.println("Car Model: " + model + ", Car Color: " + color + ", Engine Type: " + engine.getEngineType() + ", Engine Power: " + engine.getEnginePower());
+  }
+}
+
+/// Application A -> PostgreeSql -> SQL
---
+++ SpringFundamentals3/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Main {
+  public static void main(String[] args) {
+
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    Car car = context.getBean(Car.class);
+    System.out.println("Car 1 hashcode: " + car.hashCode());
+    car.printCar();
+  }
+}
